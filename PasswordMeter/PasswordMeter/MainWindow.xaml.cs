fde6880cfde05df402477c3962811782ad1ad777
jordi-mena <12402754@student.pxl.be>
2024-10-21 11:12:00 +0200

    passwordmeter

--- PasswordMeter/PasswordMeter/MainWindow.xaml.cs
+++ PasswordMeter/PasswordMeter/MainWindow.xaml.cs
@@ -30,7 +30,99 @@ namespace PasswordMeter
             InitializeComponent();
         }
 
+
+
         private void passwordMeterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            string tempUser = userNameTextBox.Text.Trim();
+            string temPasswoord = passwordTextBox.Text.Trim();  //hebben we deze var nodig???
+            int failedCounter = 0;
+
+
+
+            resultTextBlock.Text = string.Empty;
+            //controleer of het ww niet de gebruikersnaam bevat (contains = zoals bool)
+           bool checkUsercontainer = temPasswoord.Contains(tempUser);
+
+            //als het true is dan moet je een melding laten zien op de resulttextbox
+
+            //Controleer of het wachtwoord niet de gebruikersnaam bevat.
+            if (checkUsercontainer == true )
+            {
+                resultTextBlock.Text = "wachtwoord mag gebruikersnaam NIET bevatten!\n";
+           
+            }
+
+
+            
+            //Controleer of het wachtwoord minstens één getal bevat.
+            if (temPasswoord.Any(char.IsNumber) == false)
+            {
+                failedCounter++;
+                //resultTextBlock.Text += "Wachtwoord moet minstens een getal bevatten!\n";
+            
+            }
+            if (temPasswoord.Length < 10)
+            {
+                failedCounter ++;
+                //resultTextBlock.Text += "Wachtwoord moet minstens tien karakters lang zijn!\n";
+
+            }
+            switch (failedCounter) 
+            { 
+           
+                case 0:
+                    resultTextBlock.Text = "your password is strong";
+                    break;
+                case 1:
+                    resultTextBlock.Text = "your password is OK";
+                    break;
+
+                default:
+                    resultTextBlock.Text = "your passwoord is bad";
+                    break;
+
+                
+
+            
+            
+            }
+
+            //Haal 5 willekeurige letters uit de gebruikersnaam en voeg ze toe als kleine letters aan het wachtwoord. (random)
+            //Genereer een getal van één tot vijf en voeg zoveel uitroeptekens toe.(stringbuilder)
+
+            //Genereer 5 willekeurige cijfers en voeg deze toe aan het wachtwoord. (random)
+            //Haal 2 willekeurige letters uit de gebruikersnaam en voeg ze toe als hoofdletters aan het wachtwoord.(random)
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void passwordTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void userNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
